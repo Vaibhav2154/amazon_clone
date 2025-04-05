a6b67ab0caf39f120a006603d182cb2899e55399
Vaibhav2154 <vaibhavvaibhu2005@gmail.com>
2025-04-05 18:02:20 +0530

    saved orders

--- scripts/checkout/paymentsummary.js
+++ scripts/checkout/paymentsummary.js
@@ -2,6 +2,7 @@ import { cart } from "../../data/cart.js";
 import { products, getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import { formatCurrency as fm } from "../utils/money.js";
+import { addOrder } from "../../data/orders.js";
 
 export function renderPaymentSummary() {
     let productPriceCents = 0;
@@ -57,11 +58,32 @@ export function renderPaymentSummary() {
         <div class="payment-summary-money">${fm(totalCents)}</div>
         </div>
 
-        <button class="place-order-button button-primary">
+        <button class="place-order-button button-primary js-place-order">
         Place your order
         </button>
     `;
 
     document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHtml;
-    
+
+    const placeOrderButton = document.querySelector('.js-place-order')
+        .addEventListener('click', async () => {
+
+            try {
+                const response = await fetch('https://supersimplebackend.dev/orders', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        cart,
+                    })
+                })
+                const order = await response.json();
+                addOrder(order);
+            } catch (error) {
+                console.error('Error placing order:', error);
+                alert('There was an error placing your order. Please try again later.');
+            }
+            window.location.href = 'orders.html';
+        });
 }
